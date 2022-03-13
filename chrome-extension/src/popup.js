f6eab6554c4500155d84c3581ef94e9ffb7273e9
Rastislav Hepner <rastislav.hepner@gmail.com>
2022-03-13 15:17:22 +0100

    Complete refactor of popup.js

--- chrome-extension/src/popup.js
+++ chrome-extension/src/popup.js
@@ -1,51 +1,65 @@
-injectContentScript();
+(async function() {
+  let tabId = await getCurrentTabId();
+  let hasScript = await hasContentScript(tabId);
+
+  if (!hasScript) {
+    await injectContentScript(tabId, "src/content.js");
+    await injectStyles(tabId, "src/grid-overlay.css");
+  }
+})();
 
 // identify tab, that it's running in
-async function getCurrentTab() {
+async function getCurrentTabId() {
   let queryOptions = { active: true, currentWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+  return tab.id;
 }
 
-// injects content script if there isn't one already
-async function injectContentScript() {
-  let currentTab = await getCurrentTab();
-  let msgObj = { message: "ping" };
+// resolves as true if content script is already injected
+function hasContentScript(tabId) {
+  return new Promise((resolve, reject) => {
+    let msgObj = { message: "ping" };
+    chrome.tabs.sendMessage(tabId, msgObj, response => {
+      if (!response) {
+        chrome.runtime.lastError && resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+}
 
-  // ping tab's content scripts
-  // Note: sendMessage doesn't support promises yet
-  chrome.tabs.sendMessage(currentTab.id, msgObj, response => {
-    // when no reply => inject script
-    if (!response && chrome.runtime.lastError) {
-      console.log("Content script not detected, injecting...");
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: currentTab.id },
-          files: ["src/content.js"]
-        },
-        result => {
-          if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-          } else {
-            console.log("Content script injected");
-          }
-        }
-      );
-      chrome.scripting.insertCSS(
-        {
-          target: { tabId: currentTab.id },
-          files: ["src/grid-overlay.css"]
-        },
-        result => {
-          if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-          } else {
-            console.log("CSS injected");
-          }
-        }
-      );
-    } else {
-      console.log("Content script detected, skipping injection");
-    }
+// let details = { target: { tabId: currentTab.id }, files: ["src/content.js"] };
+function injectContentScript(tabId, filePath) {
+  return new Promise((resolve, reject) => {
+    let details = {
+      target: { tabId: tabId },
+      files: [filePath]
+    };
+
+    chrome.scripting.executeScript(details, injectionResult => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(injectionResult);
+      }
+    });
+  });
+}
+
+function injectStyles(tabId, filePath) {
+  return new Promise((resolve, reject) => {
+    let details = {
+      target: { tabId: tabId },
+      files: [filePath]
+    };
+
+    chrome.scripting.insertCSS(details, () => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve();
+      }
+    });
   });
 }
