b7be66fd5c0e82765e06617aba13e86ea1ce6c23
Rastislav Hepner <rastislav.hepner@gmail.com>
2022-03-22 15:04:08 +0100

    iife encasulates function definitions

--- chrome-extension/src/popup.js
+++ chrome-extension/src/popup.js
@@ -16,161 +16,163 @@
 
   let elements = await getOverlayElements(tabId);
   updateElementsInDOM(elements, tabId);
-})();
 
-function isChromeUrl(url) {
-  return /^chrome:\/\//.test(url);
-}
+  function isChromeUrl(url) {
+    return /^chrome:\/\//.test(url);
+  }
 
-function toggleOverlayElement(checkboxElem, tabId) {
-  let msgObj = { index: checkboxElem.dataset.index };
-  if (checkboxElem.checked) {
-    msgObj["message"] = "add";
-    chrome.tabs.sendMessage(tabId, msgObj);
-  } else {
-    msgObj["message"] = "remove";
+  function toggleOverlayElement(checkboxElem, tabId) {
+    let msgObj = { index: checkboxElem.dataset.index };
+    if (checkboxElem.checked) {
+      msgObj["message"] = "add";
+      chrome.tabs.sendMessage(tabId, msgObj);
+    } else {
+      msgObj["message"] = "remove";
+      chrome.tabs.sendMessage(tabId, msgObj);
+    }
+  }
+
+  function toggleAllOverlays(checkboxElem, tabId) {
+    let msgObj = { message: "addAll" };
+    let checkboxes = Array.from(
+      document.querySelectorAll(".bootstrap-element")
+    );
+    if (checkboxElem.checked) {
+      msgObj["isChecked"] = true;
+      // check all checkboxes which are not checked yet
+      checkboxes.reverse().forEach(checkbox => {
+        if (!checkbox.checked) {
+          checkbox.click();
+        }
+      });
+    } else {
+      msgObj["isChecked"] = false;
+      // uncheck all checkboxes which are currently checked
+      checkboxes.reverse().forEach(checkbox => {
+        if (checkbox.checked) {
+          checkbox.click();
+        }
+      });
+    }
     chrome.tabs.sendMessage(tabId, msgObj);
   }
-}
 
-function toggleAllOverlays(checkboxElem, tabId) {
-  let msgObj = { message: "addAll" };
-  let checkboxes = Array.from(document.querySelectorAll(".bootstrap-element"));
-  if (checkboxElem.checked) {
-    msgObj["isChecked"] = true;
-    // check all checkboxes which are not checked yet
-    checkboxes.reverse().forEach(checkbox => {
-      if (!checkbox.checked) {
-        checkbox.click();
-      }
-    });
-  } else {
-    msgObj["isChecked"] = false;
-    // uncheck all checkboxes which are currently checked
-    checkboxes.reverse().forEach(checkbox => {
-      if (checkbox.checked) {
-        checkbox.click();
-      }
-    });
+  function updateElementsInDOM(elements, tabId) {
+    elementList = document.querySelector(".element-list");
+
+    if (elements.length > 1) {
+      hideNoElementsMsg();
+      elementList.innerHTML = createListOfElementsHTML(elements);
+
+      // Add event listeners to bootstrap element checkboxes
+      let checkboxes = document.querySelectorAll(".bootstrap-element");
+      checkboxes.forEach(node => {
+        node.addEventListener("change", function(event) {
+          toggleOverlayElement(this, tabId);
+        });
+      });
+
+      // Add event listener for all checkbox
+      let all = document.querySelector("#checkbox-all");
+      all.addEventListener("change", function(event) {
+        toggleAllOverlays(this, tabId);
+      });
+    }
   }
-  chrome.tabs.sendMessage(tabId, msgObj);
-}
 
-function updateElementsInDOM(elements, tabId) {
-  elementList = document.querySelector(".element-list");
+  function hideNoElementsMsg() {
+    document.querySelector(".no-elements").style.display = "none";
+  }
 
-  if (elements.length > 1) {
-    hideNoElementsMsg();
-    elementList.innerHTML = createListOfElementsHTML(elements);
-
-    // Add event listeners to bootstrap element checkboxes
-    let checkboxes = document.querySelectorAll(".bootstrap-element");
-    checkboxes.forEach(node => {
+  function addActionToCheckboxes(nodelist, tabId) {
+    Array.from(nodelist).forEach(node => {
       node.addEventListener("change", function(event) {
         toggleOverlayElement(this, tabId);
       });
     });
+  }
 
-    // Add event listener for all checkbox
-    let all = document.querySelector("#checkbox-all");
-    all.addEventListener("change", function(event) {
-      toggleAllOverlays(this, tabId);
+  function createListOfElementsHTML(elements) {
+    let allState = elements.pop()[1] ? "checked" : "";
+    let resultHTML = `<div><input type="checkbox" id="checkbox-all" ${allState}><label for="checkbox-all">all</label></div>`;
+
+    elements.forEach((element, index) => {
+      let id = `checkbox${index}`;
+      let name = element[0];
+      let elementState = element[1] ? "checked" : "";
+      resultHTML += `<div><input type="checkbox" class="bootstrap-element" id="${id}" data-index="${index}" ${elementState}><label for="${id}">${name}</label></div>`;
+    });
+
+    return resultHTML;
+  }
+
+  function getOverlayElements(tabId) {
+    return new Promise((resolve, reject) => {
+      let msgObj = { message: "list" };
+      chrome.tabs.sendMessage(tabId, msgObj, response => {
+        if (!response) {
+          reject(chrome.runtime.lastError);
+        } else if (!response.list) {
+          reject(new Error("content script did not reply list message"));
+        } else {
+          resolve(response.list);
+        }
+      });
     });
   }
-}
 
-function hideNoElementsMsg() {
-  document.querySelector(".no-elements").style.display = "none";
-}
+  // identify tab, that it's running in
+  async function getCurrentTabId() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
 
-function addActionToCheckboxes(nodelist, tabId) {
-  Array.from(nodelist).forEach(node => {
-    node.addEventListener("change", function(event) {
-      toggleOverlayElement(this, tabId);
+  // resolves as true if content script is already injected
+  function hasContentScript(tabId) {
+    return new Promise((resolve, reject) => {
+      let msgObj = { message: "ping" };
+      chrome.tabs.sendMessage(tabId, msgObj, response => {
+        if (!response) {
+          chrome.runtime.lastError && resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
     });
-  });
-}
+  }
 
-function createListOfElementsHTML(elements) {
-  let allState = elements.pop()[1] ? "checked" : "";
-  let resultHTML = `<div><input type="checkbox" id="checkbox-all" ${allState}><label for="checkbox-all">all</label></div>`;
+  function injectContentScript(tabId, filePath) {
+    return new Promise((resolve, reject) => {
+      let details = {
+        target: { tabId: tabId },
+        files: [filePath]
+      };
 
-  elements.forEach((element, index) => {
-    let id = `checkbox${index}`;
-    let name = element[0];
-    let elementState = element[1] ? "checked" : "";
-    resultHTML += `<div><input type="checkbox" class="bootstrap-element" id="${id}" data-index="${index}" ${elementState}><label for="${id}">${name}</label></div>`;
-  });
-
-  return resultHTML;
-}
-
-function getOverlayElements(tabId) {
-  return new Promise((resolve, reject) => {
-    let msgObj = { message: "list" };
-    chrome.tabs.sendMessage(tabId, msgObj, response => {
-      if (!response) {
-        reject(chrome.runtime.lastError);
-      } else if (!response.list) {
-        reject(new Error("content script did not reply list message"));
-      } else {
-        resolve(response.list);
-      }
+      chrome.scripting.executeScript(details, injectionResult => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(injectionResult);
+        }
+      });
     });
-  });
-}
+  }
 
-// identify tab, that it's running in
-async function getCurrentTabId() {
-  let queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
+  function injectStyles(tabId, filePath) {
+    return new Promise((resolve, reject) => {
+      let details = {
+        target: { tabId: tabId },
+        files: [filePath]
+      };
 
-// resolves as true if content script is already injected
-function hasContentScript(tabId) {
-  return new Promise((resolve, reject) => {
-    let msgObj = { message: "ping" };
-    chrome.tabs.sendMessage(tabId, msgObj, response => {
-      if (!response) {
-        chrome.runtime.lastError && resolve(false);
-      } else {
-        resolve(true);
-      }
+      chrome.scripting.insertCSS(details, () => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve();
+        }
+      });
     });
-  });
-}
-
-function injectContentScript(tabId, filePath) {
-  return new Promise((resolve, reject) => {
-    let details = {
-      target: { tabId: tabId },
-      files: [filePath]
-    };
-
-    chrome.scripting.executeScript(details, injectionResult => {
-      if (chrome.runtime.lastError) {
-        reject(chrome.runtime.lastError);
-      } else {
-        resolve(injectionResult);
-      }
-    });
-  });
-}
-
-function injectStyles(tabId, filePath) {
-  return new Promise((resolve, reject) => {
-    let details = {
-      target: { tabId: tabId },
-      files: [filePath]
-    };
-
-    chrome.scripting.insertCSS(details, () => {
-      if (chrome.runtime.lastError) {
-        reject(chrome.runtime.lastError);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
+  }
+})();
